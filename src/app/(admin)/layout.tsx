b6561c4a195b94856dd2be02fe30fd058a9063d2
Subodh Rana <subodhrana390@gmail.com>
2025-07-04 18:24:15 +0000

    Error: In HTML, <html> cannot be a child of <main>. This will cause a hy

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,6 +1,4 @@
 import type { Metadata } from "next";
-import "../globals.css";
-import { Toaster } from "@/components/ui/toaster";
 import {
   Sidebar,
   SidebarProvider,
@@ -20,35 +18,18 @@ export default function AdminRootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        ></link>
-      </head>
-      <body className="font-body antialiased bg-background">
-        <AuthProvider>
-          <AuthGuard>
-            <SidebarProvider>
-                <Sidebar>
-                    <AdminSidebar />
-                </Sidebar>
-                <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-                    <AdminHeader />
-                    <main className="p-4 sm:px-6 sm:py-0">{children}</main>
-                </div>
-            </SidebarProvider>
-          </AuthGuard>
-        </AuthProvider>
-        <Toaster />
-      </body>
-    </html>
+    <AuthProvider>
+      <AuthGuard>
+        <SidebarProvider>
+            <Sidebar>
+                <AdminSidebar />
+            </Sidebar>
+            <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+                <AdminHeader />
+                <main className="p-4 sm:px-6 sm:py-0">{children}</main>
+            </div>
+        </SidebarProvider>
+      </AuthGuard>
+    </AuthProvider>
   );
 }
