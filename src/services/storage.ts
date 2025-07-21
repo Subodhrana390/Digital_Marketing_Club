22ed334e7e6a2505e5be4902d0c786700119c3ce
Subodh Rana <subodhrana390@gmail.com>
2025-07-21 02:45:36 +0000

    fixed cloudinary

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -11,8 +11,7 @@ import { Readable } from 'stream';
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 async function uploadStream(buffer: Buffer, options: any): Promise<any> {
